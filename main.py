8036efd88d5aefa9245212ec6970a475ff23cb30
Carlos Ortega González <75342750+cortega26@users.noreply.github.com>
2023-03-17 12:46:41 -0300

    PEP-8 changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from languages import languages
 
 
 def download_pdf(url):
-    """Downloads a PDF file from a given URL."""
+    """Download a PDF file from a given URL."""
     response = requests.get(url)
     response.raise_for_status()
     with open("temp.pdf", "wb") as f:
@@ -18,7 +18,7 @@ def download_pdf(url):
 
 
 def pdf_to_text(pdf_file):
-    """Converts a PDF file to text."""
+    """Convert a PDF file to text."""
     pdf = PyPDF2.PdfReader(pdf_file)
     text = ""
     for page in range(len(pdf.pages)):
@@ -27,15 +27,9 @@ def pdf_to_text(pdf_file):
 
 
 def remove_stop_words(text, language):
-    """Removes stop words from a given text for a given language."""
-    for lang in nltk.corpus.stopwords.fileids():
-        try:
-            nltk.corpus.stopwords.words(lang)
-        except LookupError:
-            nltk.download('stopwords', quiet=True)
-    language = languages.get(language).lower()
+    """Remove stop words from a given text for a given language."""
     try:
-        stop_words = set(stopwords.words(language))
+        stop_words = set(stopwords.words(language.lower()))
         words = nltk.word_tokenize(text)
         filtered_text = [word for word in words if word.casefold() not in stop_words]
         return ' '.join(filtered_text)
@@ -45,20 +39,20 @@ def remove_stop_words(text, language):
 
 
 def count_words(text):
-    """Counts the occurrences of each word in a given text."""
+    """Count the occurrences of each word in a given text."""
     words = re.findall(r'\b\w+\b', text)
     word_counts = dict(Counter(words).most_common(10))
     return word_counts
 
 
 def count_word_or_phrase(text, word_or_phrase):
-    """Counts the number of occurrences of a given word or phrase in a given text."""
+    """Count the number of occurrences of a given word or phrase in a given text."""
     count = text.count(word_or_phrase)
     return count
 
 
 def detect_language(text):
-    """Detects the language of a given text."""
+    """Detect the language of a given text."""
     try:
         language = detect(text)
         return language
@@ -68,7 +62,7 @@ def detect_language(text):
 
 def main(url, word_or_phrase):
     """
-    Runs the main program, which downloads a PDF file from a given URL, converts it to text,
+    Run the main program, which downloads a PDF file from a given URL, converts it to text,
     removes stop words, and counts the occurrences of the top 10 most common words and a given
     word or phrase in the text.
     """
@@ -76,14 +70,14 @@ def main(url, word_or_phrase):
     text = pdf_to_text("temp.pdf")
     language = detect_language(text)
 
-    if language is not None:
+    if language:
         filtered_text = remove_stop_words(text, language)
         word_counts = count_words(filtered_text)
         word_or_phrase_count = count_word_or_phrase(text, word_or_phrase)
 
         print(f"Language: {language}")
         print("Top 10 Words:")
-        for word, count in list(word_counts.items()):
+        for word, count in word_counts.items():
             print(f"{word}: {count}")
         print(f"Occurrences of '{word_or_phrase}': {word_or_phrase_count}")
     else:
