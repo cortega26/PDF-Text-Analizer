988dac5b03fdf5a6dd7638731243fb7ee0ab54c4
Carlos Ortega <carlosortega77@gmail.com>
2024-07-03 19:08:12 -0400

    Remove trailing newline character at end of file.

--- pdf_processor.py
+++ pdf_processor.py
@@ -178,4 +178,3 @@ class PdfProcessor:
 if __name__ == "__main__":
     pdf_processor = PdfProcessor("https://antilogicalism.com/wp-content/uploads/2017/07/atlas-shrugged.pdf")
     print(pdf_processor.main('Who is John Galt?'))
-
